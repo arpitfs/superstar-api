e62da90fee42e7a3e8610021c493b6e7633156ca
arpitfs <amarpitmalik204@gmail.com>
2020-12-30 00:56:01 +0530

    Implementation of api-key authentication

---
+++ Authentication/ApiKeyAuthAttribute.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+namespace ApiWorld.Authentication
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class ApiKeyAuthAttribute : Attribute, IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            //to get the api-key from QueryString
+            //context.HttpContext.Request.Query["api-key"]
+            if (!context.HttpContext.Request.Headers.TryGetValue("api-key", out var apiKeyFromRequest))
+            {
+                context.Result = new UnauthorizedResult();
+                // here the middleware will short circut and not forward request is made
+                return;
+            }
+
+            var configuration = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+            var apiKey = configuration.GetValue<string>("api-key");
+            if(apiKey != apiKeyFromRequest)
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            // the below code is used to call the next middlerware
+            await next();
+        }
+    }
+}
---
+++ Controllers/V1/ApiKeyController.cs
@@ -0,0 +1,15 @@
+﻿using ApiWorld.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiWorld.Controllers.V1
+{
+    [ApiKeyAuthAttribute]
+    public class ApiKeyController : ControllerBase
+    {
+        [HttpGet("api/secret")]
+        public IActionResult Get()
+        {
+            return Ok("Api key authentication");
+        }
+    }
+}
